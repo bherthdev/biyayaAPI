4cda56f3afd68f86cc05e1390686cd45dee88fa9
bherth <bherthchavez@gmail.com>
2024-07-29 02:20:41 +0800

    for vercel frontend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     // 'http://localhost:5173'
-    // 'https://biyaya-bherthchavezs-projects.vercel.app'
-    'https://biyaya.onrender.com'
+    // 'https://biyaya.onrender.com'
+    'https://biyaya-bherthchavezs-projects.vercel.app'
 ]
 
 module.exports = allowedOrigins
