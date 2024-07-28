01f53937b139faf73a27bb83009f6373c38c9ee6
bherth <bherthchavez@gmail.com>
2024-07-29 01:29:47 +0800

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     // 'http://localhost:5173'
-    'https://biyaya-bherthchavezs-projects.vercel.app'
+    // 'https://biyaya-bherthchavezs-projects.vercel.app'
+    'https://biyaya.onrender.com'
 ]
 
 module.exports = allowedOrigins
